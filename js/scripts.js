faf6211897e9bebbbcaebe3b5c0fe7f2aa223758
WayneMusungu <waynemuyera17@gmail.com>
2022-03-13 22:09:32 +0300

    Add hover to the portfolio section

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,12 @@ $(document).ready(function(){
         $(this).children(".hide").slideToggle(2500)
     });
 });
+//portifolio section 
+var projHovers = ['project1', 'project2', 'project3', 'project4', 'project5', 'project6', 'project7', 'project8']
+$(document).ready(function(){
+    projHovers.forEach(function(projHover){
+        $('#'+projHover).hover(
+          function(){$('#'+projHover+' p').fadeIn()},
+          function(){$('#'+projHover+' p').fadeOut()})
+      });
+});
