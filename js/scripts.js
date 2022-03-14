cad74b42773b81928e73fe6bbb5968263ab17a38
WayneMusungu <waynemuyera17@gmail.com>
2022-03-14 10:30:36 +0300

    Make adjustments to class ProjName in styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
 // EFFECTS ON WHAT WE DO
 $(document).ready(function(){
     $(".col-md-4 ").click(function(){
-        $(this).children(".hide").slideToggle(2500)
+        // create a variable speed for our slideToggle 
+        var speed= 3000
+        $(this).children(".hide").slideToggle(speed)
     });
 });
 //portifolio section 
 var projHovers = ['work1', 'work2', 'work3', 'work4', 'work5', 'work6', 'work7', 'work8']
 $(document).ready(function(){
     projHovers.forEach(function(projHover){
+        // Create a variable speed for fade
+        var speed=3000
         $('#'+projHover).hover(
-          function(){$('#'+projHover+' p').fadeIn()},
-          function(){$('#'+projHover+' p').fadeOut()})
+          function(){$('#'+projHover+' p').fadeIn(speed)},
+          function(){$('#'+projHover+' p').fadeOut(speed)})
       });
 });
 // Collecting user's input on the contact us form
